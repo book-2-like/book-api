24bade525acf05bb79f33027a7cfafa7cd0e1b9e
LeeHyeonha <lej5769@gmail.com>
2025-07-14 15:56:17 +0900

    feat: 로그 조회 기능 페이지 관련 param 요청 방식 변경

--- src/main/java/com/example/book_api/domain/log/controller/LogController.java
+++ src/main/java/com/example/book_api/domain/log/controller/LogController.java
@@ -28,23 +28,25 @@ public class LogController {
     /**
      * 저장된 로그를 조회하는 기능
      *
+     * @param page 페이지 번호
+     * @param size 한 페이지 내 요소 수
      * @param userId 유저 id 검색
      * @param targetType 도메인 타입 검색
      * @param startAt 시작일 검색
      * @param endAt 종료일 검색
-     * @param pageable 페이지 정보
      * @return 페이징 처리된 응답 Dto
      */
     @GetMapping("/logs")
     public ResponseEntity<ApiResponse<Page<LogResponseDto>>> getLogs(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size,
             @RequestParam(required = false) Long userId,
             @RequestParam(required = false) String targetType,
             @RequestParam(required = false) String startAt,
-            @RequestParam(required = false) String endAt,
-            Pageable pageable
+            @RequestParam(required = false) String endAt
     ) {
 
-        Page<LogResponseDto> logs = logService.getLogs(userId, targetType, startAt, endAt, pageable);
+        Page<LogResponseDto> logs = logService.getLogs(page, size, userId, targetType, startAt, endAt);
 
         return ApiResponse.success(
                 HttpStatus.OK, "로그 조회가 완료되었습니다.", logs);
--- src/main/java/com/example/book_api/domain/log/service/LogService.java
+++ src/main/java/com/example/book_api/domain/log/service/LogService.java
@@ -6,6 +6,7 @@ import com.example.book_api.domain.log.entity.Log;
 import com.example.book_api.domain.log.repository.LogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -36,18 +37,19 @@ public class LogService {
     /**
      * 저장된 로그를 조회하는 기능
      *
+     * @param page 페이지 번호
+     * @param size 한 페이지 내 요소 수
      * @param userId 유저 id 검색
      * @param targetType 도메인 타입 검색
      * @param startAt 시작일 검색
      * @param endAt 종료일 검색
-     * @param pageable 페이지 정보
      * @return 페이징 처리된 응답 Dto
      */
-    public Page<LogResponseDto> getLogs(Long userId, String targetType, String startAt, String endAt,
-                                        Pageable pageable) {
+    public Page<LogResponseDto> getLogs(int page, int size,
+                                        Long userId, String targetType, String startAt, String endAt) {
         LogRequestDto requestDto = new LogRequestDto(userId, targetType, startAt, endAt);
 
-
+        Pageable pageable = PageRequest.of(page-1, size);
         Page<Log> logs = logRepository.findByFilter(requestDto, pageable);
 
         return logs.map(log -> new LogResponseDto(
